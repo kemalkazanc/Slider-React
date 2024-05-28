25940f373419d247792cb220db8b2fd0fe147e31
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-05-28 13:54:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Slider from "./components/Slider/Slider";
+import SliderAuto from "./components/Slider/SliderAuto";
+
+function App() {
+  return (
+    <>
+      <Slider />
+    </>
+  );
+}
+
+export default App;
